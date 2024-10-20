5b0b2ca443d074c29946049c62c3bc256041f574
nfikri9 <nabilfikri.business@gmail.com>
2024-10-20 21:41:03 +0800

    Adding notes and functionality about padding and border

--- Notes-CSS.js
+++ Notes-CSS.js
@@ -1,8 +1,18 @@
-function myBoxModel() {
-  const myInput = {
+function myGlobal() {
+  const globalInput = {
     myHeight: document.getElementById("Input-Height").value,
-    myWidth: document.getElementById("Input-Width").value
+    myWidth: document.getElementById("Input-Width").value,
+    myPadding: document.getElementById("Input-Padding").value,
+    myBorder: document.getElementById("Input-Border").value, 
+    myBorderRadius: document.getElementById("Input-Border-Radius").value
   };
-  document.getElementById("Box-Model").style.height = myInput.myHeight;
-  document.getElementById("Box-Model").style.width = myInput.myWidth;
+  return globalInput;
+}
+
+function myBoxModel(myInput) {
+  document.getElementById("Box-Model").style.height = myGlobal(myInput).myHeight;
+  document.getElementById("Box-Model").style.width = myGlobal(myInput).myWidth;
+  document.getElementById("Box-Model").style.padding = myGlobal(myInput).myPadding;
+  document.getElementById("Box-Model").style.border = myGlobal(myInput).myBorder;
+  document.getElementById("Box-Model").style.borderRadius = myGlobal(myInput).myBorderRadius;
 }
