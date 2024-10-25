50d317348d2ce22f90f817ca021a28e36a4f7fa8
nfikri9 <nabilfikri.business@gmail.com>
2024-10-25 21:33:22 +0800

    Add notes and functionality for flex, grid, justify and align properties

--- Notes-CSS.js
+++ Notes-CSS.js
@@ -40,7 +40,22 @@ function myGlobal() {
     myFlexShrink: document.getElementById("Input-Flex-Shrink").value,
     myFlexBasis: document.getElementById("Input-Flex-Basis").value,
     myFlexDirection: document.getElementById("Input-Flex-Direction").value,
-    myFlexWrap: document.getElementById("Input-Flex-Wrap").value
+    myFlexWrap: document.getElementById("Input-Flex-Wrap").value,
+    myGridTemplateRows: document.getElementById("Input-Grid-Template-Rows").value,
+    myGridTemplateColumns: document.getElementById("Input-Grid-Template-Columns").value,
+    myGridTemplateAreas: document.getElementById("Input-Grid-Template-Areas").value,
+    myGridAutoRows: document.getElementById("Input-Grid-Auto-Rows").value,
+    myGridAutoColumns: document.getElementById("Input-Grid-Auto-Columns").value,
+    myGridAutoFlow: document.getElementById("Input-Grid-Auto-Flow").value,
+    myGridRowStart: document.getElementById("Input-Grid-Row-Start").value,
+    myGridColumnStart: document.getElementById("Input-Grid-Column-Start").value,
+    myGridRowEnd: document.getElementById("Input-Grid-Row-End").value,
+    myGridColumnEnd: document.getElementById("Input-Grid-Column-End").value,
+    myJustifyContent: document.getElementById("Input-Justify-Content").value,
+    myJustifyItems: document.getElementById("Input-Justify-Items").value,
+    myAlignContent: document.getElementById("Input-Align-Content").value,
+    myAlignItems: document.getElementById("Input-Align-Items").value,
+    myAlignSelf: document.getElementById("Input-Align-Self").value
   };
   return globalInput;
 }
@@ -79,6 +94,21 @@ function ClickHandlerFlexShrink(myInput) { document.getElementsByClassName("Chil
 function ClickHandlerFlexBasis(myInput) { document.getElementsByClassName("Child-Box")[0].style.flexBasis = myGlobal(myInput).myFlexBasis; }
 function ClickHandlerFlexDirection(myInput) { document.getElementsByClassName("Parent-Box")[0].style.flexDirection = myGlobal(myInput).myFlexDirection; }
 function ClickHandlerFlexWrap(myInput) { document.getElementsByClassName("Parent-Box")[0].style.flexWrap = myGlobal(myInput).myFlexWrap; }
+function ClickHandlerGridTemplateRows(myInput) { document.getElementsByClassName("Parent-Box")[0].style.gridTemplateRows = myGlobal(myInput).myGridTemplateRows; }
+function ClickHandlerGridTemplateColumns(myInput) { document.getElementsByClassName("Parent-Box")[0].style.gridTemplateColumns = myGlobal(myInput).myGridTemplateColumns; }
+function ClickHandlerGridTemplateAreas(myInput) { document.getElementsByClassName("Parent-Box")[0].style.gridTemplateAreas = myGlobal(myInput).myGridTemplateAreas; }
+function ClickHandlerGridAutoRows(myInput) { document.getElementsByClassName("Parent-Box")[0].style.gridAutoRows = myGlobal(myInput).myGridAutoRows; }
+function ClickHandlerGridAutoColumns(myInput) { document.getElementsByClassName("Parent-Box")[0].style.gridAutoColumns = myGlobal(myInput).myGridAutoColumns; }
+function ClickHandlerGridAutoFlow(myInput) { document.getElementsByClassName("Parent-Box")[0].style.gridAutoFlow = myGlobal(myInput).myGridAutoFlow; }
+function ClickHandlerGridRowStart(myInput) { document.getElementsByClassName("Child-Box")[0].style.gridRowStart = myGlobal(myInput).myGridRowStart; }
+function ClickHandlerGridColumnStart(myInput) { document.getElementsByClassName("Child-Box")[0].style.gridColumnStart = myGlobal(myInput).myGridColumnStart; }
+function ClickHandlerGridRowEnd(myInput) { document.getElementsByClassName("Child-Box")[0].style.gridRowEnd = myGlobal(myInput).myGridRowEnd; }
+function ClickHandlerGridColumnEnd(myInput) { document.getElementsByClassName("Child-Box")[0].style.gridColumnEnd = myGlobal(myInput).myGridColumnEnd; }
+function ClickHandlerJustifyContent(myInput) { document.getElementsByClassName("Parent-Box")[0].style.justifyContent = myGlobal(myInput).myJustifyContent; }
+function ClickHandlerJustifyItems(myInput) { document.getElementsByClassName("Parent-Box")[0].style.justifyItems = myGlobal(myInput).myJustifyItems; }
+function ClickHandlerAlignContent(myInput) { document.getElementsByClassName("Parent-Box")[0].style.alignContent = myGlobal(myInput).myAlignContent; }
+function ClickHandlerAlignItems(myInput) { document.getElementsByClassName("Parent-Box")[0].style.alignItems = myGlobal(myInput).myAlignItems; }
+function ClickHandlerAlignSelf(myInput) { document.getElementsByClassName("Parent-Box")[0].style.alignSelf = myGlobal(myInput).myAlignSelf; }
 
 function myBoxModel(myInput) {
   document.getElementById("Box-Model").style.height = myGlobal(myInput).myHeight;
