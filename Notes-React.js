b163cb3b870214fe7b10e80248585ce972170e93
nfikri9 <nabilfikri.business@gmail.com>
2024-11-20 21:36:21 +0800

    Adding note for React

--- Notes-React.js
+++ Notes-React.js
@@ -8,3 +8,36 @@ function ReactIntroduction() {
   }
   else { myVisibility.style.display = "none"; }
 }
+
+///////////////////////
+// REACT JSX ELEMENT //
+///////////////////////
+function ReactJSXElement() {
+  let myVisibility = document.getElementById("React-JSX-Element");
+  if (myVisibility.style.display === "none") {
+    myVisibility.style.display = "block";
+  }
+  else { myVisibility.style.display = "none"; }
+}
+
+//////////////////////////////////////////
+// REACT STATELESS FUNCTIONAL COMPONENT //
+//////////////////////////////////////////
+function ReactStatelessFunctionalComponent() {
+  let myVisibility = document.getElementById("React-Stateless-Functional-Component");
+  if (myVisibility.style.display === "none") {
+    myVisibility.style.display = "block";
+  }
+  else { myVisibility.style.display = "none"; }
+}
+
+///////////////////////////////
+// REACT STATELESS COMPONENT //
+///////////////////////////////
+function ReactStatelessComponent() {
+  let myVisibility = document.getElementById("React-Stateless-Component");
+  if (myVisibility.style.display === "none") {
+    myVisibility.style.display = "block";
+  }
+  else { myVisibility.style.display = "none"; }
+}
