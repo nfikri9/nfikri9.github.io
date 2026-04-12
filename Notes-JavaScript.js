f9c0edcf92978a17fe5b2526dd786b1a71fa9d6a
nfikri9 <nabilfikri.business@gmail.com>
2026-04-12 22:01:34 +0800

    Update notes for JS

--- Notes-JavaScript.js
+++ Notes-JavaScript.js
@@ -26,129 +26,46 @@ function SearchJavaScriptReferences() {
   }
 }
 
-////////////////////////////////////////////////////////////////////////// LIST OF JS METHODS ////////////////////////////////////////////////////////////////////////////
-function FillMethod() {
-  let FillMethod = document.getElementById("Fill-Method");
+//////////////////////////////////////////////////////////////////////////////// PROPERTY ////////////////////////////////////////////////////////////////////////////////
+function ConstructorProperty() {
+  let ConstructorProperty = document.getElementById("Constructor-Property");
   let JavaScriptReferences = document.getElementById("JavaScript-References");
 
-  if (FillMethod.style.display === "block") {
-    FillMethod.style.display = "none";
+  if (ConstructorProperty.style.display === "block") {
+    ConstructorProperty.style.display = "none";
     JavaScriptReferences.style.display = "block";
   }
   else {
-    FillMethod.style.display = "block";
+    ConstructorProperty.style.display = "block";
     JavaScriptReferences.style.display = "none";
   }
 }
 
-function JoinMethod() {
-  let JoinMethod = document.getElementById("Join-Method");
+function EpsilonProperty() {
+  let EpsilonProperty = document.getElementById("Epsilon-Property");
   let JavaScriptReferences = document.getElementById("JavaScript-References");
 
-  if (JoinMethod.style.display === "block") {
-    JoinMethod.style.display = "none";
+  if (EpsilonProperty.style.display === "block") {
+    EpsilonProperty.style.display = "none";
     JavaScriptReferences.style.display = "block";
   }
   else {
-    JoinMethod.style.display = "block";
+    EpsilonProperty.style.display = "block";
     JavaScriptReferences.style.display = "none";
   }
 }
 
-function PopMethod() {
-  let PopMethod = document.getElementById("Pop-Method");
+///////////////////////////////////////////////////////////////////////////////// METHOD /////////////////////////////////////////////////////////////////////////////////
+function IsFiniteMethod() {
+  let IsFiniteMethod = document.getElementById("IsFinite-Method");
   let JavaScriptReferences = document.getElementById("JavaScript-References");
 
-  if (PopMethod.style.display === "block") {
-    PopMethod.style.display = "none";
+  if (IsFiniteMethod.style.display === "block") {
+    IsFiniteMethod.style.display = "none";
     JavaScriptReferences.style.display = "block";
   }
   else {
-    PopMethod.style.display = "block";
-    JavaScriptReferences.style.display = "none";
-  }
-}
-
-function PushMethod() {
-  let PushMethod = document.getElementById("Push-Method");
-  let JavaScriptReferences = document.getElementById("JavaScript-References");
-
-  if (PushMethod.style.display === "block") {
-    PushMethod.style.display = "none";
-    JavaScriptReferences.style.display = "block";
-  }
-  else {
-    PushMethod.style.display = "block";
-    JavaScriptReferences.style.display = "none";
-  }
-}
-
-function ReverseMethod() {
-  let ReverseMethod = document.getElementById("Reverse-Method");
-  let JavaScriptReferences = document.getElementById("JavaScript-References");
-
-  if (ReverseMethod.style.display === "block") {
-    ReverseMethod.style.display = "none";
-    JavaScriptReferences.style.display = "block";
-  }
-  else {
-    ReverseMethod.style.display = "block";
-    JavaScriptReferences.style.display = "none";
-  }
-}
-
-function ShiftMethod() {
-  let ShiftMethod = document.getElementById("Shift-Method");
-  let JavaScriptReferences = document.getElementById("JavaScript-References");
-
-  if (ShiftMethod.style.display === "block") {
-    ShiftMethod.style.display = "none";
-    JavaScriptReferences.style.display = "block";
-  }
-  else {
-    ShiftMethod.style.display = "block";
-    JavaScriptReferences.style.display = "none";
-  }
-}
-
-function ToReversedMethod() {
-  let ToReversedMethod = document.getElementById("ToReversed-Method");
-  let JavaScriptReferences = document.getElementById("JavaScript-References");
-
-  if (ToReversedMethod.style.display === "block") {
-    ToReversedMethod.style.display = "none";
-    JavaScriptReferences.style.display = "block";
-  }
-  else {
-    ToReversedMethod.style.display = "block";
-    JavaScriptReferences.style.display = "none";
-  }
-}
-
-function UnshiftMethod() {
-  let UnshiftMethod = document.getElementById("Unshift-Method");
-  let JavaScriptReferences = document.getElementById("JavaScript-References");
-
-  if (UnshiftMethod.style.display === "block") {
-    UnshiftMethod.style.display = "none";
-    JavaScriptReferences.style.display = "block";
-  }
-  else {
-    UnshiftMethod.style.display = "block";
-    JavaScriptReferences.style.display = "none";
-  }
-}
-
-function WithMethod() {
-  let WithMethod = document.getElementById("With-Method");
-  let JavaScriptReferences = document.getElementById("JavaScript-References");
-
-  if (WithMethod.style.display === "block") {
-    WithMethod.style.display = "none";
-    JavaScriptReferences.style.display = "block";
-  }
-  else {
-    WithMethod.style.display = "block";
+    IsFiniteMethod.style.display = "block";
     JavaScriptReferences.style.display = "none";
   }
 }
